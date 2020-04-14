23d1cd1c55c793fd74fdd7da866893ca7d1423b4
nitunwa <nitun.2@gmail.com>
2020-04-14 13:54:34 -0700

    add  test check if row has no repeating values

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -22,17 +22,25 @@ describe('Game', () => {
   var game;
   beforeEach(() => {
     game = new Game();
-    var array1 = [1,2,3,4,5,6,7,8,9];
+    var array1 = [7,1,6,5,2,3,8,4,9];
+    var array2 = [5,8,9,4,7,1,2,3,6];
+    var array3 = [4,3,2,6,9,8,5,1,7];
+    var array4 = [1,5,7,3,8,6,9,2,4];
+    var array5 = [8,2,4,9,5,7,3,6,1];
+    var array6 = [6,9,3,1,4,2,7,8,5];
+    var array7 = [2,4,8,7,1,9,6,5,3];
+    var array8 = [9,6,1,2,3,5,4,7,8];
+    var array9 = [3,7,5,8,6,4,1,9,2];
     
     var a = new Row(array1);
-    var b = new Row(array1);
-    var d = new Row(array1);
-    var c = new Row(array1);
-    var e = new Row(array1);
-    var f = new Row(array1);
-    var g = new Row(array1);
-    var h = new Row(array1);
-    var i = new Row(array1);
+    var b = new Row(array2);
+    var c = new Row(array3);
+    var d = new Row(array4);
+    var e = new Row(array5);
+    var f = new Row(array6);
+    var g = new Row(array7);
+    var h = new Row(array8);
+    var i = new Row(array9);
     game.addRow(a);
     game.addRow(b);
     game.addRow(c);
@@ -46,22 +54,19 @@ describe('Game', () => {
 
   });
   
+
   test('should correctly add row to game object',
   () => {
     
-    
-    
-    
-    
-    expect(game.rows[0].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[1].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[2].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[3].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[4].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[5].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[6].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[7].r).toEqual([1,2,3,4,5,6,7,8,9]);
-    expect(game.rows[8].r).toEqual([1,2,3,4,5,6,7,8,9]);
+    expect(game.rows[0].r).toEqual([7,1,6,5,2,3,8,4,9]);
+    expect(game.rows[1].r).toEqual([5,8,9,4,7,1,2,3,6]);
+    expect(game.rows[2].r).toEqual([4,3,2,6,9,8,5,1,7]);
+    expect(game.rows[3].r).toEqual([1,5,7,3,8,6,9,2,4]);
+    expect(game.rows[4].r).toEqual([8,2,4,9,5,7,3,6,1]);
+    expect(game.rows[5].r).toEqual([6,9,3,1,4,2,7,8,5]);
+    expect(game.rows[6].r).toEqual([2,4,8,7,1,9,6,5,3]);
+    expect(game.rows[7].r).toEqual([9,6,1,2,3,5,4,7,8]);
+    expect(game.rows[8].r).toEqual([3,7,5,8,6,4,1,9,2]);
 
   });
 
@@ -69,13 +74,14 @@ describe('Game', () => {
     expect(game.rows.length).toEqual(9);
     
   }); 
+  
 
   test('should correctly check if row has no repeating values', function(){
-
+    expect(game.findDuplicate(0)).toEqual(false);
   });
 
   test('should correctly check if all 9 rows have no repeating values', function(){
-
+    
   });
 
   test('should correctly check if column has no repeating values', function(){
--- src/sudoku.js
+++ src/sudoku.js
@@ -7,6 +7,26 @@ Game.prototype.addRow = function(row) {
   this.rows.push(row);
 }
 
+Game.prototype.checkSudoku = function() {
+  //for (var i = 0; i < rows.length; i++)
+  //  
+}
+
+Game.prototype.findDuplicate = function(rowNum){
+  
+  for(var i=0; i<this.rows.length;i++){
+    for(var j=i+1; j<this.rows.length;j++){
+      if(this.rows[rowNum].r[i] ===this.rows[rowNum].r[j] ){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+  // function findDuplicate(arrs,row){
+      
+  // }
+
 export function Row(r) {
   this.r = r
 }
