403bc23aa7696c74709da5552da46ffbb4373b31
nitunwa <nitun.2@gmail.com>
2020-04-14 09:41:25 -0700

    add initial  setup with test env environment

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Sudoku Checker_
+
+#### Creates a sudoku puzzle that checks the user's answers and return whether or not the user's solution is a valid solution._, 4/14/2020_
+
+#### By _**DJ Zevenbergen & Nitun Datta**_
+
+## Description
+
+_This web application uses webpack, jest, babel, objects, prototypes, looping, arrays to recieve a user's sudoku solution, then determines whether or not the solution is valid._
+
+## Setup/Installation Requirements
+
+* Download entire repo
+* Open index.html in browser
+* If it doesn't work, it may require having Bootstrap 3.3.7 CSS file in CSS folder and jQuery 3.4.1 JS file in JS folder
+
+## Specs
+
+* - The program recieves 
+    * Input: "Large", "Cheese", "Pepperoni", "Anchovies"
+    * Output: "Pizza 1, "Large", "Cheese", "Pepperoni", "Anchovies"
+
+  
+
+## Known Bugs
+-
+- 
+
+
+## Access to Website
+
+
+
+## License
+
+Copyright © 2020
+
+**_DJ Zevenbergen & Nitun Datta_**
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //new line
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',  // new line
+  devServer: {                 // new line
+    contentBase: './dist'      // new line
+  },
+
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'Ping Pong',
+  //     template: './src/index.html',
+  //     inject: 'body'
+  //   })
+  // ],
+  // plugins: [
+  //   new CleanWebpackPlugin(),   // new line
+  //   new HtmlWebpackPlugin({
+  //     title: 'Journal',
+  //     template: './src/index.html',
+  //     inject: 'body'
+  //   })
+  // ],
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),   // new line
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'sudoku-checker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
